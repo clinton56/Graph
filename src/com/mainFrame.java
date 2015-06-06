1ab5b27acdada08448a52bbc1429cfda9946f72b
Mariusz <saviola44marian@gmail.com>
2015-06-06 18:57:00 +0200

    change view of options panel

--- src/com/mainFrame.java
+++ src/com/mainFrame.java
@@ -30,9 +30,29 @@ public class mainFrame extends JFrame {
 
     mxGraph graph;
     Object[] V;
+    int nbOfVertex;
 
-    private void button2ActionPerformed(ActionEvent e) {
-        int n = Integer.parseInt(textField1.getText());
+    private void button3ActionPerformed(ActionEvent e) {
+        int v1 = Integer.parseInt(JtextV1.getText());
+        int v2 = Integer.parseInt(JtextV2.getText());
+        graph.insertEdge(graph.getDefaultParent(), null, "df", V[v1 - 1], V[v2 - 1]);
+        mxGraphComponent graphComponent = new mxGraphComponent(graph);
+        graphPanel.removeAll();
+        graphPanel.add(graphComponent);
+        graphPanel.setAutoscrolls(true);
+        graphPanel.setPreferredSize(graphComponent.getPreferredSize());
+        graphPanel.revalidate();
+        graphPanel.repaint();
+        pack();
+        revalidate();
+        repaint();
+
+    }
+
+
+    private void newGraphButtonActionPerformed(ActionEvent e) {
+        int n = Integer.parseInt(numberOfVertex.getText());
+        nbOfVertex=n;
         graph = new mxGraph();
         Object parent = graph.getDefaultParent();
         Map<String, Object> st = graph.getStylesheet().getDefaultVertexStyle();
@@ -46,7 +66,7 @@ public class mainFrame extends JFrame {
         //circleLayout.execute(parent);
         circleLayout.execute(parent);
         graph.getModel().beginUpdate();
-        V = new Object[n];
+        V = new Object[100];
 
         try {
             /*Object v1 = graph.insertVertex(parent, null, "Hello", 10, 20, 80,
@@ -94,10 +114,57 @@ public class mainFrame extends JFrame {
         repaint();
     }
 
-    private void button3ActionPerformed(ActionEvent e) {
-        int v1 = Integer.parseInt(startEdgeText.getText());
-        int v2 = Integer.parseInt(endEdgeText.getText());
-        graph.insertEdge(graph.getDefaultParent(), null, "df", V[v1 - 1], V[v2 - 1]);
+    private void addVertexButtonActionPerformed(ActionEvent e) {
+        int n = Integer.parseInt(numberOfVertex.getText());
+        Object parent = graph.getDefaultParent();
+        Map<String, Object> st = graph.getStylesheet().getDefaultVertexStyle();
+        st.put(mxConstants.STYLE_SHAPE, mxConstants.SHAPE_ELLIPSE);
+        st.put(mxConstants.STYLE_PERIMETER, mxPerimeter.EllipsePerimeter);
+        st.put(mxConstants.STYLE_GRADIENTCOLOR, "white");
+        st.put(mxConstants.STYLE_FONTSIZE, "10");
+        mxFastOrganicLayout layout = new mxFastOrganicLayout(graph);
+        layout.setForceConstant(80);
+        mxCircleLayout circleLayout = new mxCircleLayout(graph);
+        //circleLayout.execute(parent);
+        circleLayout.execute(parent);
+        graph.getModel().beginUpdate();
+        //V = new Object[nbOfVertex];
+
+        try {
+            /*Object v1 = graph.insertVertex(parent, null, "Hello", 10, 20, 80,
+                    30);
+            Object v2 = graph.insertVertex(parent, null, "World!", 240, 250,
+                    80, 30);
+            Object v3 = graph.insertVertex(parent, null, "World!", 300, 50,
+                    80, 30);
+            Object v4 = graph.insertVertex(parent, null, "World!", 400, 180,
+                    80, 30);
+            Object v5 = graph.insertVertex(parent, null, "World!", 450, 10,
+                    80, 30);
+            Object v6 = graph.insertVertex(parent, null, "World!", 200, 100,
+                    80, 30);
+            Object v7 = graph.insertVertex(parent, null, "World!", 10, 310,
+                    80, 80);*/
+
+            for (int i = nbOfVertex; i < nbOfVertex+n; i++) {
+                V[i] = graph.insertVertex(parent, null, "" + (i + 1), 0, 0, 30, 30);
+            }
+            nbOfVertex+=n;
+            //graph.setCellStyles(mxConstants.STYLE_FILLCOLOR, "#10C500", new Object[]{v1, v2});
+            //graph.setCellStyle(mxConstants.SHAPE_ELLIPSE, new Object[]{v4});
+
+
+            /*graph.insertEdge(parent, null, "Edge", v1, v2);
+            graph.insertEdge(parent, null, "Edge", v1, v3);
+            graph.insertEdge(parent, null, "Edge", v2, v6);
+            graph.insertEdge(parent, null, "Edge", v6, v4);
+            graph.insertEdge(parent, null, "Edge", v5, v3);*/
+            // circleLayout.execute(parent);
+            circleLayout.execute(parent);
+            //graph.setCellStyles(mxConstants.STYLE_FILLCOLOR, "#10C500", new Object[]{v1, v2});
+        } finally {
+            graph.getModel().endUpdate();
+        }
         mxGraphComponent graphComponent = new mxGraphComponent(graph);
         graphPanel.removeAll();
         graphPanel.add(graphComponent);
@@ -108,8 +175,14 @@ public class mainFrame extends JFrame {
         pack();
         revalidate();
         repaint();
-
     }
+
+    private void deleteButtonActionPerformed(ActionEvent e) {
+        graph.removeCells(graph.getSelectionCells());
+        nbOfVertex--;
+    }
+
+
 
 
     private void initComponents() {
@@ -118,23 +191,23 @@ public class mainFrame extends JFrame {
         DefaultComponentFactory compFactory = DefaultComponentFactory.getInstance();
         optionsPanel = new JPanel();
         panel2 = new JPanel();
-        label1 = new JLabel();
-        textField1 = new JTextField();
-        button1 = new JButton();
-        button2 = new JButton();
+        vertexLabel = new JLabel();
+        numberOfVertex = new JTextField();
+        newGraphButton = new JButton();
+        addVertexButton = new JButton();
         panel1 = new JPanel();
         deleteLab1 = new JLabel();
         deleteLab2 = new JLabel();
         deleteButton = new JButton();
         edgeLabel = compFactory.createLabel("text");
-        addEdgePanel = new JPanel();
-        startEdgeText = new JTextField();
-        endEdgeText = new JTextField();
-        addEdge = new JButton();
-        algorithmsPanel = new JPanel();
-        selectAlgorithmLabel = compFactory.createLabel("text");
+        panel10 = new JPanel();
+        JtextV1 = new JTextField();
+        JtextV2 = new JTextField();
+        button3 = new JButton();
+        panel11 = new JPanel();
+        label5 = compFactory.createLabel("text");
         selectAlgorithm = new JComboBox();
-        algorithmButton = new JButton();
+        button4 = new JButton();
         graphPanel = new JPanel();
 
         //======== this ========
@@ -163,84 +236,101 @@ public class mainFrame extends JFrame {
                     "default",
                     "3*(default, $lgap), default"));
 
-                //---- label1 ----
-                label1.setText("Liczba wierzcho\u0142k\u00f3w:");
-                panel2.add(label1, CC.xy(1, 1));
-                panel2.add(textField1, CC.xy(1, 3));
+                //---- vertexLabel ----
+                vertexLabel.setText("Liczba wierzcho\u0142k\u00f3w:");
+                panel2.add(vertexLabel, CC.xy(1, 1));
+                panel2.add(numberOfVertex, CC.xy(1, 3));
 
-                //---- button1 ----
-                button1.setText("Nowy graf");
-                panel2.add(button1, CC.xy(1, 5));
+                //---- newGraphButton ----
+                newGraphButton.setText("Nowy graf");
+                panel2.add(newGraphButton, CC.xy(1, 5));
 
-                //---- button2 ----
-                button2.setText("Dodaj");
-                panel2.add(button2, CC.xy(1, 7));
+                //---- addVertexButton ----
+                addVertexButton.setText("Dodaj");
+                addVertexButton.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        addVertexButtonActionPerformed(e);
+                    }
+                });
+                panel2.add(addVertexButton, CC.xy(1, 7));
             }
             optionsPanel.add(panel2, CC.xy(1, 1));
 
             //======== panel1 ========
             {
-                deletePanel.setLayout(new FormLayout(
+                panel1.setLayout(new FormLayout(
                     "default",
                     "2*(default, $lgap), default"));
 
                 //---- deleteLab1 ----
-                deleteLab1.setText("Zaznacz krawedz");
-                deletePanel.add(deleteLab1, CC.xy(1, 1));
+                deleteLab1.setText("Podaj krawedz");
+                panel1.add(deleteLab1, CC.xy(1, 1));
 
                 //---- deleteLab2 ----
                 deleteLab2.setText("lub wierzcholek:");
-                deletePanel.add(deleteLab2, CC.xy(1, 3));
+                panel1.add(deleteLab2, CC.xy(1, 3));
 
                 //---- deleteButton ----
                 deleteButton.setText("Usun");
-                deletePanel.add(deleteButton, CC.xy(1, 5));
+                deleteButton.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        deleteButtonActionPerformed(e);
+                    }
+                });
+                panel1.add(deleteButton, CC.xy(1, 5));
             }
-            optionsPanel.add(deletePanel, CC.xy(1, 3));
+            optionsPanel.add(panel1, CC.xy(1, 3));
 
             //---- edgeLabel ----
             edgeLabel.setText("Dodaj krawedz:");
             optionsPanel.add(edgeLabel, CC.xy(1, 5));
 
-            //======== addEdgePanel ========
+            //======== panel10 ========
             {
-                addEdgePanel.setLayout(new FormLayout(
+                panel10.setLayout(new FormLayout(
                     "default, $lcgap, default",
                     "default"));
 
-                //---- startEdgeText ----
-                startEdgeText.setPreferredSize(new Dimension(50, 26));
-                startEdgeText.setMinimumSize(new Dimension(50, 26));
-                addEdgePanel.add(startEdgeText, CC.xy(1, 1));
+                //---- JtextV1 ----
+                JtextV1.setPreferredSize(new Dimension(50, 26));
+                JtextV1.setMinimumSize(new Dimension(50, 26));
+                panel10.add(JtextV1, CC.xy(1, 1));
 
-                //---- endEdgeText ----
-                endEdgeText.setMinimumSize(new Dimension(50, 26));
-                endEdgeText.setPreferredSize(new Dimension(50, 26));
-                addEdgePanel.add(endEdgeText, CC.xy(3, 1));
+                //---- JtextV2 ----
+                JtextV2.setMinimumSize(new Dimension(50, 26));
+                JtextV2.setPreferredSize(new Dimension(50, 26));
+                panel10.add(JtextV2, CC.xy(3, 1));
             }
-            optionsPanel.add(addEdgePanel, CC.xy(1, 7));
+            optionsPanel.add(panel10, CC.xy(1, 7));
 
-            //---- addEdge ----
-            addEdge.setText("Dodaj");
-            addEdge.addActionListener(e -> button3ActionPerformed(e));
-            optionsPanel.add(addEdge, CC.xy(1, 9));
+            //---- button3 ----
+            button3.setText("Dodaj");
+            button3.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    button3ActionPerformed(e);
+                }
+            });
+            optionsPanel.add(button3, CC.xy(1, 9));
 
-            //======== algorithmsPanel ========
+            //======== panel11 ========
             {
-                algorithmsPanel.setLayout(new FormLayout(
+                panel11.setLayout(new FormLayout(
                     "default",
                     "2*(default, $lgap), default"));
 
-                //---- selectAlgorithmLabel ----
-                selectAlgorithmLabel.setText("Wybierz algorytm");
-                algorithmsPanel.add(selectAlgorithmLabel, CC.xy(1, 1));
-                algorithmsPanel.add(selectAlgorithm, CC.xy(1, 3));
+                //---- label5 ----
+                label5.setText("Wybierz algorytm");
+                panel11.add(label5, CC.xy(1, 1));
+                panel11.add(selectAlgorithm, CC.xy(1, 3));
 
-                //---- algorithmButton ----
-                algorithmButton.setText("Start");
-                algorithmsPanel.add(algorithmButton, CC.xy(1, 5));
+                //---- button4 ----
+                button4.setText("Start");
+                panel11.add(button4, CC.xy(1, 5));
             }
-            optionsPanel.add(algorithmsPanel, CC.xy(1, 11));
+            optionsPanel.add(panel11, CC.xy(1, 11));
         }
         contentPane.add(optionsPanel, CC.xy(1, 1, CC.DEFAULT, CC.TOP));
 
@@ -257,25 +347,26 @@ public class mainFrame extends JFrame {
     }
 
     // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
-    // Generated using JFormDesigner Evaluation license - Damian Liwak
+    // Generated using JFormDesigner Evaluation license - Mariusz Bielec
     private JPanel optionsPanel;
-    private JPanel panel8;
-    private JLabel numOfVertexLabel;
-    private JTextField nrOfVertex;
-    private JButton okButton;
-    private JPanel deletePanel;
+    private JPanel panel2;
+    private JLabel vertexLabel;
+    private JTextField numberOfVertex;
+    private JButton newGraphButton;
+    private JButton addVertexButton;
+    private JPanel panel1;
     private JLabel deleteLab1;
     private JLabel deleteLab2;
     private JButton deleteButton;
     private JLabel edgeLabel;
-    private JPanel addEdgePanel;
-    private JTextField startEdgeText;
-    private JTextField endEdgeText;
-    private JButton addEdge;
-    private JPanel algorithmsPanel;
-    private JLabel selectAlgorithmLabel;
+    private JPanel panel10;
+    private JTextField JtextV1;
+    private JTextField JtextV2;
+    private JButton button3;
+    private JPanel panel11;
+    private JLabel label5;
     private JComboBox selectAlgorithm;
-    private JButton algorithmButton;
+    private JButton button4;
     private JPanel graphPanel;
     // JFormDesigner - End of variables declaration  //GEN-END:variables
 }
